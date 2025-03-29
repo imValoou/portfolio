07a4b1938b89644ea1fdf0cec7f41ea9afabe0bc
imValoou <valentinbenedet@gmail.com>
2025-03-29 19:35:17 +0100

    aller on reprend tout depuis le début sur des bonnes bases

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,40 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { Store } from './store';
 
 @Component({
-	selector: 'root',
-	imports: [RouterOutlet, HeaderComponent, FooterComponent],
-	templateUrl: './app.component.html',
-	styleUrl: './app.component.scss',
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-	title = 'portfolio';
-	private store = inject(Store);
-	private translate = inject(TranslateService);
-
-	constructor() {
-		this.translate.addLangs(['en', 'fr']);
-		this.translate.setDefaultLang('en');
-		this.setCurrentLang();
-	}
-
-	prepareRoute(outlet: RouterOutlet) {
-		return outlet?.activatedRouteData?.['animation'];
-	}
-
-	setCurrentLang() {
-		this.store.setLang(this.getBrowserLang());
-	}
-
-	getBrowserLang(): string {
-		return (
-			this.translate.getBrowserLang()?.toLocaleLowerCase() ??
-			this.translate.getDefaultLang().toLocaleLowerCase()
-		);
-	}
+  title = 'portfolio';
 }
--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -1,39 +1,9 @@
-import { HttpClient, provideHttpClient } from '@angular/common/http';
-import {
-	ApplicationConfig,
-	importProvidersFrom,
-	provideZoneChangeDetection,
-} from '@angular/core';
-import {
-	provideClientHydration,
-	withEventReplay,
-} from '@angular/platform-browser';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { AppCommonModule } from './app.common.module';
 import { routes } from './app.routes';
-
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (
-	http: HttpClient
-) => new TranslateHttpLoader(http, './i18n/', '.json');
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideHttpClient(),
-		importProvidersFrom([
-			TranslateModule.forRoot({
-				loader: {
-					provide: TranslateLoader,
-					useFactory: httpLoaderFactory,
-					deps: [HttpClient],
-				},
-			}),
-		]),
-		provideRouter(routes),
-		provideClientHydration(withEventReplay()),
-		importProvidersFrom(AppCommonModule),
-	],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };

98a0f7e2f46760ebbc3f8f9a3f8cb9bcf483756b
imValoou <valentinbenedet@gmail.com>
2025-04-25 17:55:43 +0200

    Add global styles and layout for Next.js application with Tailwind CSS integration
    - Created globals.css for global styles including dark mode support
    - Implemented layout.tsx to define the root layout with custom fonts
    - Developed page.tsx as the main page structure with responsive design and links

--- portfolio/src/app/components/cart/cart.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pf-cart',
-  imports: [],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss',
-})
-export class CartComponent {}
--- portfolio/src/app/components/contact/contact.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'pf-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
-})
-export class ContactComponent {}

e00018152f979379677e87d5eaace5000e819835
imValoou <valentinbenedet@gmail.com>
2025-05-12 23:03:24 +0200

    potite coquille

--- portfolio/src/sections/about-me-banner.tsx
+++ portfolio/src/sections/about-me-banner.tsx
@@ -3,7 +3,7 @@ import AngledSeparator from '@/components/angled-separator';
 export default function AboutMeBanner() {
 	return (
 		<section className="primary relative h-96 flex flex-col items-center justify-center gap-3 text-center">
-			<div className="flex flex-col gap-2 max-w-xlpa">
+			<div className="flex flex-col gap-2 max-w-xl">
 				<h1 className="font-bold ">About me</h1>
 				<h2>From full-stack dev to parachutist artilleryman</h2>
 			</div>
